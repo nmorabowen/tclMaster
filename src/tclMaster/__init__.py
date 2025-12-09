8b6e9ab391a8835f3aa5c7bbe1a9eafe084bc230
Nicolas Mora Bowen <nmorabowen@gmail.com>
2025-12-09 10:36:19 -0300

    add tcl preprocesor and batch replace function

--- src/tclMaster/__init__.py
+++ src/tclMaster/__init__.py
@@ -2,9 +2,11 @@ from .core import tclMaster
 
 from .utilities import utilities
 from .utilities import TclPreprocessor
+from .utilities.batch_replace import batch_replace
 
 __all__ = [
     "tclMaster",
     "utilities",
     "TclPreprocessor",
+    'batch_replace',
 ]
--- src/tclMaster/utilities/__init__.py
+++ src/tclMaster/utilities/__init__.py
@@ -1,7 +1,9 @@
 from .utilities import utilities
 from .TclPreprocessor import TclPreprocessor
+from .batch_replace import batch_replace
 
 __all__ = [
     "utilities",
     "TclPreprocessor",
+    "batch_replace",
 ]
